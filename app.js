4a4148d94772805ad1b466f57065a92af0134dc8
sutr90 <jnovacho@yahoo.com>
2013-04-06 22:44:08 +0200

    Uprava portu.

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -30,6 +29,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(process.env.OPENSHIFT_INTERNAL_PORT, process.env.OPENSHIFT_INTERNAL_IP, function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
