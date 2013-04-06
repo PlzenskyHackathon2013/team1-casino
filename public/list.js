1e74f980b97047341137375e21933a7f2e63fb3d
Petr Hribal <emailbox@petrhribal.net>
2013-04-07 01:47:27 +0200

    Opravena chybna domena v list.js

--- public/list.js
+++ public/list.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var chat_comet = io.connect('http://localhost:8080');
+  var chat_comet = io.connect('http://firstapp-casino.ph:8080');
   chat_comet.on('msg', function (msg) {
     // replace the contents of the div 
     $('#chat_msgs').html('<div>' + msg.text + '</div>');
