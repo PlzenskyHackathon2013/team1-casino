82e0cb6ccf9403360175c6ad222636bc6a0cc705
cervotoc <cervotoc@outlook.com>
2013-04-06 20:09:46 +0200

    Pridani testovani validniho nicku na vstup. Pripraveny (zakomentovany) fce pro identifikaci prihlasenyho hrace.

--- routes/list.js
+++ routes/list.js
@@ -8,15 +8,23 @@ var fs = require('fs');
  */
 
 exports.get_page = function(req, res){
-    res.set('Content-Type', 'text/html');
-    res.send(fs.readFileSync('./html/list.html'));
+
+    //if(isValidID(req.cookies.id)) { // kontrola validniho hrace 
+      res.set('Content-Type', 'text/html');
+      res.send(fs.readFileSync('./html/list.html'));
+    //} else
+    /*
+      res.set('Content-Type', 'text/html');
+      res.send(fs.readFileSync('./html/login.html'));
+    */
 };
 
 exports.post_page = function(req, res){
-
-    if(req.body.nick == 'cervotoc') // je validni nick
+    var vNick = '/^([A-Za-z0-9]){3,15}$/';
+    if(vNick.test(req.body.nick)==true) // je validni nick
     {
-      res.cookie('id', 'cervotoc');
+      //res.cookie('id', getPlayerID(req.body.nick));
+      res.cookie('nick', req.body.nick);
       res.set('Content-Type', 'text/html');
       res.send(fs.readFileSync('./html/list.html'));
     } else
