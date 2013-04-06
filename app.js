3d4d4f15c25235211f9fdaf4311ed4e9e9a9da0e
Petr Hribal <emailbox@petrhribal.net>
2013-04-06 21:28:52 +0200

    Pridany session. Zaveden entry point access logger.

--- app.js
+++ app.js
@@ -3,17 +3,21 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 var server = require('./http_server');
+var log_access_routes = require('./routes/log_access');
 var login_routes = require('./routes/login');
 var list_routes = require('./routes/list');
 
 var http_server = new server.http_server;
+
 //http_server.app.set('env', 'localhost-dev-env');
 http_server.initialize();
 
 // All routes must be placed here.
+
+// Access Log
+//http_server.app.get('/*', log_access_routes.do_log);
 http_server.app.get('/', login_routes.get_page);
 http_server.app.get('/login', login_routes.get_page);
 http_server.app.get('/list', list_routes.get_page);
--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 /*
  * GET login page.
  */
-
 exports.get_page = function(req, res){
     res.set('Content-Type', 'text/html');
     res.send(fs.readFileSync('./html/login.html'));
