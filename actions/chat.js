fc51a9b4bc1fcdddb99733ce1eb073cfbb56ebcb
Petr Hribal <emailbox@petrhribal.net>
2013-04-07 02:21:02 +0200

    Pridan chat.

--- actions/chat.js
+++ actions/chat.js
@@ -1,9 +1,11 @@
 exports.on_connection = function (socket) {
     console.warn('chat commet conection active');
     socket.emit('msg', {
-        text: 'hi'
+        text: 'Welcome'
     });
     socket.on('msg', function (data) {
-        console.log(data);
+        socket.emit('msg', {
+            text: data.text
+        });
     });
 };
--- public/list.js
+++ public/list.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
   var chat_comet = io.connect('http://firstapp-casino.ph');
+  //var chat_comet = io.connect('http://localhost:8080');
+
   chat_comet.on('msg', function (msg) {
     // replace the contents of the div 
     $('#chat_msgs').html('<div>' + msg.text + '</div>');
+  });
+
+  $("#chat_msg_send").click(function() {
+    var text = $('#chat_msg_text').val(); 
+    chat_comet.emit('msg', {
+        text: text
+    });
   });
 });
